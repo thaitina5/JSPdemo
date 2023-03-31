ba6d1201b4a1b976f127a8e4edff4efb28ac8de9
thait <thait@TIN_XPS>
2023-03-31 23:30:26 +0700

    Giới thiệu,tạo Servlet và chuyển hướng trang

---
+++ JSP_21/src/main/java/controller/DangNhap.java
@@ -0,0 +1,57 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DangNhap
+ */
+@WebServlet("/dang-nhap")
+public class DangNhap extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public DangNhap() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		String url="";
+		if(username.equalsIgnoreCase("bukudao") && password.equals("123")) {
+			url="/success.jsp";
+		} else {
+			url="/error.jsp";
+		}
+		
+		RequestDispatcher rd = getServletContext().getRequestDispatcher(url);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Method doPost");
+		doGet(request, response);
+	}
+
+}
